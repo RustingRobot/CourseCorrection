ff4c5a2018885cdf0350868b4347f342b6ad209c
RustingRobot <RustingRobot@protonmail.com>
2024-05-20 05:07:23 +0200

    test2

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello' })
-}
+import axios from 'axios';
+
+export default async (req, res) => {
+  console.log("test");
+  console.log(req.url);
+  const { data } = await axios.get(`https://api.scratch.mit.edu/users/RustingRobot`);
+  res.json(data);
+};
