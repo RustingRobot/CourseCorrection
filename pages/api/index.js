15fdbe57e122457e930149e1681cae2b557e4303
RustingRobot <RustingRobot@protonmail.com>
2024-05-20 13:03:26 +0200

    decode URI

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,6 +4,6 @@ export default async (req, res) => {
   console.log("test");
   console.log(req.url);
   console.log(decodeURIComponent(req.url.slice(5)));
-  const { data } = await axios.get(`${decodeURIComponent(req.url.slice(5))}`);
+  const { data } = await axios.get(`${decodeURIComponent(req.url.slice(5, -1))}`);
   res.json(data);
 };
