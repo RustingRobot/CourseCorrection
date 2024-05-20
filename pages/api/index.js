07aa94a8ee473627117efbbf04be2712024f94d9
RustingRobot <RustingRobot@protonmail.com>
2024-05-20 05:04:20 +0200

    test

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export default async (req, res) => {
+  console.log("test");
+  console.log(req.url);
   const { data } = await axios.get(`${req.url.slice(6)}`);
   res.json(data);
 };
