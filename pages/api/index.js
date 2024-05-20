6364f6d369e2fe04379c7098730ff6dc005328a9
RustingRobot <RustingRobot@protonmail.com>
2024-05-20 05:19:11 +0200

    decode URI

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default async (req, res) => {
   console.log("test");
   console.log(req.url);
-  const { data } = await axios.get(`${decodeURI(req.url.slice(5))}`);
+  console.log(decodeURIComponent(req.url.slice(5)));
+  const { data } = await axios.get(`${decodeURIComponent(req.url.slice(5))}`);
   res.json(data);
 };
--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async (req, res) => {
   console.log("test");
   console.log(req.url);
-  console.log(decodeURI(req.url.slice(5)))
+  console.log(decodeURIComponent(req.url.slice(5)))
   const { data } = await axios.get(`https://api.scratch.mit.edu/users/RustingRobot`);
   res.json(data);
 };
