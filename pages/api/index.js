f6a496bbfdfb201d788d3493cd312693281936a3
RustingRobot <RustingRobot@protonmail.com>
2024-05-20 13:45:39 +0200

    cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 export default async (req, res) => {
-  console.log("test");
   console.log(req.url);
   console.log(decodeURIComponent(req.url.slice(5)));
-  const { data } = await axios.get(`${decodeURIComponent(req.url.slice(5, -1))}`);
+  const { data } = await axios.get(`${decodeURIComponent(req.url.slice(5, -1))}`
+  , {
+    headers: {
+        'User-Agent': 'curl/7.68.0'
+    }
+  });
   res.json(data);
 };
--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -4,6 +4,11 @@ export default async (req, res) => {
   console.log("test");
   console.log(req.url);
   console.log(decodeURIComponent(req.url.slice(5)))
-  const { data } = await axios.get(`https://api.scratch.mit.edu/users/RustingRobot`);
+  const { data } = await axios.get(`https://api.scratch.mit.edu/users/RustingRobot`
+  , {
+    headers: {
+        'User-Agent': 'curl/7.68.0'
+    }
+  });
   res.json(data);
 };
